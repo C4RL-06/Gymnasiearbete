41b88e72f1e091e661fb256c57df608488c3fb69
Qazzman987 <2006eriksson@gmail.com>
2024-10-17 11:10:40 +0200

    Backend Communication created

--- Vibrationssensor/src/backEnd.java
+++ Vibrationssensor/src/backEnd.java
@@ -1,5 +1,44 @@
+import com.fazecast.jSerialComm.SerialPort;
+import java.io.InputStream;
+
 public class backEnd {
-    public void startBackEnd() {
+    public void startBackEnd()  {
         System.out.println("Back end started");
+        //Maybe wrong com port
+        SerialPort arduinoPort = SerialPort.getCommPort("COM3");
+        arduinoPort.setComPortParameters(9600,8,1,0);
+        arduinoPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING,1024,0);
+
+        if (!arduinoPort.openPort()){
+            System.out.println("ERROR: COM port not available");
+
+        }
+
+
+
+        try {
+            InputStream in = arduinoPort.getInputStream();
+            StringBuilder messageBuffer = new StringBuilder();
+            byte[] buffer = new byte[1024];
+            int len;
+
+            while ((len = in.read(buffer)) > 0) {
+                String receivedData = new String(buffer, 0, len);
+                messageBuffer.append(receivedData);  // Append the received data to buffer
+
+                if (receivedData.contains("\n")) {  // \n is the break which shows the code the string is finished
+                    System.out.println("Received: " + messageBuffer.toString().trim());
+                    messageBuffer.setLength(0);  // Clear the buffer
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        } finally {
+            arduinoPort.closePort();
+        }
+
+
+
     }
 }

7f49b8f857132eac83e16fec40bdd0702ae5cb01
C4RL-06 <carl.axelson2006@gmail.com>
2024-10-21 12:34:16 +0200

    Responsive Sensor locations (FRONT END)

--- Vibrationssensor/src/frontEnd.java
+++ Vibrationssensor/src/frontEnd.java
@@ -9,12 +9,11 @@ public class frontEnd {
     private static final int ORIGINAL_WIDTH = 1140;
     private static final int ORIGINAL_HEIGHT = 820;
     private static final double ASPECT_RATIO = (double) ORIGINAL_HEIGHT / ORIGINAL_WIDTH;
-    private ArrayList<ArrayList<Integer>> sensorCoordinates = new ArrayList<>();
+    private ArrayList<Point> sensorCoordinates = new ArrayList<>();
 
     JFrame window;
     JTabbedPane tabbedPane;
     JPanel mapPanel;
-    JPanel testPage;
 
     public void startFrontEnd() {
         System.out.println("Front end started");
@@ -31,63 +30,90 @@ public class frontEnd {
 
         tabbedPane = new JTabbedPane();
 
-        //Creates the map image in mapPanel
+        // Creates the map image in mapPanel
         mapPanel = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 Image mapImage = new ImageIcon("./assets/karta.png").getImage();
 
-                int centerX = (window.getWidth() - ((int) (window.getHeight()/ASPECT_RATIO))) / 2;
-                if (mapImage != null) {
+                // Get current panel dimensions and calculate scaling
+                int panelWidth = window.getWidth();
+                int panelHeight = window.getHeight();
 
-                    //Render image (-10 is a manual fix to a small blank space that was created to the left of the map)
-                    g.drawImage(mapImage, centerX - 10, 0, (int) (window.getHeight()/ASPECT_RATIO), window.getHeight(), this);
+                int imageWidth = (int) (panelHeight / ASPECT_RATIO);
+                int imageHeight = panelHeight;
+
+                int centerX = (panelWidth - imageWidth) / 2;
+
+                if (mapImage != null) {
+                    // Render the scaled image
+                    g.drawImage(mapImage, centerX, 0, imageWidth, imageHeight, this);
                 } else {
-                    //Error message if image doesn't render
+                    // Error message if image doesn't render
                     g.drawString("Image not found", 20, 20);
                 }
 
+                // Scale factor to map original image coordinates to resized image
+                double scaleX = (double) imageWidth / ORIGINAL_WIDTH;
+                double scaleY = (double) imageHeight / ORIGINAL_HEIGHT;
 
+                // Draw the red rectangles at their scaled positions
                 g.setColor(Color.RED);
-                for (ArrayList<Integer> coordinate : sensorCoordinates) {
-                    System.out.println(coordinate);
-                    int x = coordinate.get(0);
-                    int y = coordinate.get(1);
-                    g.fillRect(x - 4, y - 4, 8, 8); // -4 to center rectangle on click
+                for (Point coordinate : sensorCoordinates) {
+                    int x = (int) (coordinate.x * scaleX) + centerX;
+                    int y = (int) (coordinate.y * scaleY);
+                    g.fillRect(x - 4, y - 4, 8, 8);
                 }
             }
         };
+
+        // Add mouse listener to mapPanel
         mapPanel.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
-                ArrayList<Integer> sensorCoordinate = new ArrayList<>();
-                sensorCoordinate.add(e.getX());
-                sensorCoordinate.add(e.getY());
-                sensorCoordinates.add(sensorCoordinate);
-                mapPanel.repaint();
+                // Get current panel dimensions and calculate scaling
+                int panelWidth = window.getWidth();
+                int panelHeight = window.getHeight();
+
+                int imageWidth = (int) (panelHeight / ASPECT_RATIO);
+                int imageHeight = panelHeight;
+                int centerX = (panelWidth - imageWidth) / 2;
+
+                // Only register clicks inside the image bounds
+                if (e.getX() >= centerX && e.getX() <= centerX + imageWidth && e.getY() <= imageHeight) {
+                    // Calculate the click relative to the original image size
+                    double scaleX = (double) ORIGINAL_WIDTH / imageWidth;
+                    double scaleY = (double) ORIGINAL_HEIGHT / imageHeight;
+
+                    int imgX = (int) ((e.getX() - centerX) * scaleX);
+                    int imgY = (int) (e.getY() * scaleY);
+                    System.out.println("New sensor coordinate: " + imgX + ", " + imgY);
+
+                    // Store the coordinates relative to the original image
+                    sensorCoordinates.add(new Point(imgX, imgY));
+                    mapPanel.repaint();
+                }
             }
         });
 
-
         mapPanel.setBackground(new Color(36, 47, 62));
 
-        //Test page
-        testPage = new JPanel();
+        // Test page
+        JPanel testPage = new JPanel();
         testPage.add(new JLabel("Test page"));
         testPage.setBackground(new Color(36, 47, 62));
 
-        //Available Tabs (Row Order is Tab Index)
+        // Available Tabs (Row Order is Tab Index)
         tabbedPane.addTab("Map", mapPanel);
         tabbedPane.addTab("Test Page", testPage);
 
-        //Set the tabbedPane to fill the window
+        // Set the tabbedPane to fill the window
         window.setLayout(new BorderLayout());
         window.getContentPane().setBackground(new Color(36, 47, 62));
         window.add(tabbedPane, BorderLayout.CENTER);
         window.setVisible(true);
 
-        //This ensures the window will revalidate and repaint when resized
-        //This could be imported into this class, but we decided to write the libraries in-line
+        // Revalidate and repaint the window when resized
         window.addComponentListener(new java.awt.event.ComponentAdapter() {
             public void componentResized(java.awt.event.ComponentEvent evt) {
                 mapPanel.revalidate();

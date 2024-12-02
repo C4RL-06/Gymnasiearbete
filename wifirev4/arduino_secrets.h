907f9c0523e5296701684c908a9746ff67c45d79
Qazzman987 <66552070+Qazzman987@users.noreply.github.com>
2024-12-02 15:26:22 +0100

    Update arduino_secrets.h

--- wifirev4/arduino_secrets.h
+++ wifirev4/arduino_secrets.h
@@ -1,4 +1,2 @@
-//#define SECRET_SSID "comhem_0866BD"
-//#define SECRET_PASS "ady6284e"
-#define SECRET_SSID "ACAGuest"
-#define SECRET_PASS "FramtidNu"
+#define SECRET_SSID "SSID"
+#define SECRET_PASS "PASS"
